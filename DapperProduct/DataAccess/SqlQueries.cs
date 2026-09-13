117bcda1a3e728990310dbe316d7b319a1455582
Fidan Əsədova <fidansdova590@gmail.com>
2026-09-14 02:27:28 +0400

    commit

--- DapperProduct/DataAccess/SqlQueries.cs
+++ DapperProduct/DataAccess/SqlQueries.cs
@@ -11,6 +11,6 @@ namespace DapperProduct.DataAccess
         public const string GetByName = "SELECT * FROM Products WHERE Name=@pName";
         public const string Insert = "INSERT INTO Products([Name],[Price],[Quantity],[Description]) VALUES (@pName,@pPrice,@pQuantity,@pDescription)";
         public const string Update = "UPDATE Products SET Name=@pName,Price=@pPrice,Quantity=@pQuantity,Description=@pDescription WHERE Id=@pId";
-        public const string Delete = "DELETE From Players WHERE Id=@pId";
+        public const string Delete = "DELETE From Products WHERE Id=@pId";
     }
 }
